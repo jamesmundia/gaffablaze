8003a1c85f7fccc69e6315ce8ab388b48b027405
James Mundia <jmundia@vmware.com>
2016-12-23 13:55:49 -0500

    imported players to main.js / Added server code to insert an invalid entry and console logged error succesfully, verifying that schema is attached / adding this commit before attempting autoform addition, which is a breaking point in previous tests with blaze

--- client/main.js
+++ client/main.js
@@ -2,3 +2,6 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import '../imports/ui/body.js';
+
+import { Players } from '../imports/api/players.js';
+
--- server/main.js
+++ server/main.js
@@ -2,6 +2,13 @@ import { Meteor } from 'meteor/meteor';
 
 import '../imports/api/players.js';
 
+import { Players } from '../imports/api/players.js';
+
 Meteor.startup(() => {
   // code to run on server at startup
+
+  //Checking if schema works
+  //Players.insert ({name: "Gack Back", position: "CB"}, (error, result) => {
+  //	console.log(error)
+  //});
 });
