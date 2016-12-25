9936a4eda95314fa47dbebf7bb7e93e550b3f9bf
James Mundia <jamesmundia@gmail.com>
2016-12-25 17:32:41 -0500

    Last commit before trying AutoForm
    added game rating logic to schema and to UI. Tested click event for
    game rating, will eventually turn this into a way to update that game
    rating parameter. Will next try to leave game rating out of the
    quickForm field by using autoForm

--- client/main.js
+++ client/main.js
@@ -1,6 +1,8 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+import '../imports/startup/accounts_config.js';
+
 import '../imports/ui/body.js';
 
 import { Players } from '../imports/api/players.js';
@@ -10,5 +12,11 @@ import { Schemas } from '../imports/api/players.js';
 Template.addPlayerForm.helpers({
 	addPlayerToPlayers () {
 		return Players;
+	}
+});
+
+Template.player.events({
+	'click .rating' : function () {
+		console.log('Rating Hath Been Clickethed');
 	}
 });
--- imports/api/players.js
+++ imports/api/players.js
@@ -1,6 +1,6 @@
 import { Mongo } from 'meteor/mongo';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
- 
+
 export const Players = new Mongo.Collection('players');
 
 const Schemas = {};
@@ -8,7 +8,8 @@ const Schemas = {};
 Schemas.Player = new SimpleSchema({
 	name: {type: String, label: "Name", max: 20},
 	position: {type: String, label: "Position", max: 3},
-	rosternumber: {type: Number, label: "Roster #"}
+	rosternumber: {type: Number, label: "Roster #"},
+	gamerating: {type: Number, label: "Game Rating", optional: true}
 });
 
 Players.attachSchema(Schemas.Player);
