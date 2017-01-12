8b9de35cfa87929d1184e5564a7c0afce6fecfd1
James Mundia <jamesmundia@gmail.com>
2017-01-11 23:16:13 -0500

    added gameId regex to schema

--- client/routing.js
+++ client/routing.js
@@ -20,3 +20,12 @@ FlowRouter.route('/teams/:teamId', {
   BlazeLayout.render("mainLayout", {content: "teamPage"});
   }
 });
+
+FlowRouter.route('/teams/:teamId/games/gameId', {
+  // print params in Console
+  action: function(params, queryParams) {
+    // console.log("Params:", params);
+    // console.log("Query Params:", queryParams);
+  BlazeLayout.render("mainLayout", {content: "singleGame"});
+  }
+});
